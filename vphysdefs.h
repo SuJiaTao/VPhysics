342dde9125fb0a08f54e392f0e24a34abf1786b8
JT <60622543+HarryFishnuts@users.noreply.github.com>
2022-11-03 19:46:11 -0400

    Tweaks and changes based on updated vGraphics

--- vphysdefs.h
+++ vphysdefs.h
@@ -13,19 +13,25 @@
 
 /* ========== DEFINITIONS						==========	*/
 #define PHYSICS_OBJECTS_MAX				0x1000
-#define PHYSICS_RENDEROBJECTS_MAX		0x20
+#define PHYSICS_RENDEROBJECTS_MAX		0x08
+#define FASTMEM_STACK_BYTES				0x1000
+
+
+/* ========== TYPEDEFS							==========	*/
+typedef vPosition vVect;
+typedef vVect*    vPVect;
+typedef float	  vFloat;
 
 
 /* ========== STRUCTURES						==========	*/
 typedef struct vPhysicsMaterial
 {
-	float staticFriction;
-	float dynamicFriction;
-	float bounciness;
-	float mass;
+	vFloat staticFriction;
+	vFloat dynamicFriction;
+	vFloat bounciness;
 } vPhysicsMaterial, *vPPhysicsMaterial;
 
-typedef struct vPhysicsState
+typedef struct vPhysicsProperties
 {
 	vUI16 collideLayer;				/* bitfield of layers that object exists on. will	*/
 									/* collide w/ objects that are on same layer (any).	*/
@@ -39,28 +45,48 @@ typedef struct vPhysicsState
 	vBOOL staticPosition : 1;		/* whether the object can be moved					*/
 	vBOOL staticRotation : 1;		/* whether the object can be rotated				*/
 	vBOOL collideWithParent : 1;	/* whether the object collides w/ it's parent		*/
-} vPhysicsState, *vPPhysicsState;
+} vPhysicsProperties, *vPPhysicsProperties;
 
 typedef struct vPhysicsObject
 {
+	/* ===== PHYSICS METADATA				===== */
 	vPPhysicsObject parent;			/* parent physics object							*/
+	vUI64 age;						/* ticks spent active								*/
+	vPGRenderable visuals[PHYSICS_RENDEROBJECTS_MAX];	/* render objects				*/
 
-	vPhysicsState state;			/* simulation properties							*/
-	vPhysicsMaterial material;		/* physical properties								*/
+	/* ==== STATIC PHYSICS PROPERTIES		===== */
+	vPPhysicsProperties properties;	/* simulation properties							*/
+	vPPhysicsMaterial   material;	/* physical material properties						*/
 	
-	vRect boundingRect;				/* bounding rectangle								*/
-	vTransform2 transform;			/* object transform									*/
-
-	/* optional associated render objects				*/
-	vPRenderObject renderObjects[PHYSICS_RENDEROBJECTS_MAX];
-	
+	/* ==== OBJECT PHYSICS PROPERTIES		===== */
+	vGRect boundingRect;			/* bounding rectangle			*/
+	vTransform physicsTransform;	/* physics transform			*/
+	vFloat mass;					/* object mass					*/
+	vVect  velocity;				/* change in position			*/
+	vVect  acceleration;			/* change of change in position	*/
 
 } vPhysicsObject, *vPPhysicsObject;
+
+/* pre-allocated memory stack */
+typedef struct _vPHYSFastMemoryStack
+{
+	CRITICAL_SECTION rwLock;
+	vPBYTE block;
+	vUI64  ptr;
+} _vPHYSFastMemoryStack, *_vPPHYSFastMemoryStack;
 
 typedef struct _vPHYSInternals
 {
 	vBOOL isInitialized;
-	vHNDL physicObjectBuffer;	/* all physics objects	*/
+	CRITICAL_SECTION lock;			/* physics lock						*/
+
+	vPWorker physicsThread;			/* worker thread object				*/
+	vHNDL physObjectList;			/* dynamic list of phys objects		*/
+
+	_vPHYSFastMemoryStack fastMemStack;	/* quick allocation stack		*/
+
+
 } _vPHYSInternals, *vPPHYSInternals;
+_vPHYSInternals _vphys;	/* INSTANCE	*/
 
 #endif
